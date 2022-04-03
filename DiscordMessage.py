166872865a820315a04cf5657e97ea675d50879a
Maticula <matic.marusic@student.um.si>
2022-04-03 20:43:23 +0200

    testaaaaaaaaaaaaaaa

--- DiscordMessage.py
+++ DiscordMessage.py
@@ -3,3 +3,4 @@ from discord import Webhook, RequestsWebhookAdapter
 
 webhook = Webhook.from_url("url-https://discord.com/api/webhooks/960243712976367716/O20YxGzMUo7HOmebGMjwzvmBhUA_3kAoE-KRWoY5QLiYlL7lmQ4Ghse5TFp1hlHjLbNI", adapter=RequestsWebhookAdapter())
 webhook.send("Commit")
+print("Poslano sporocilo discordu.")
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DemoHooks
-la
+las
